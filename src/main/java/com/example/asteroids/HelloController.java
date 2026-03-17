2cec290364edee70f5a5fa210a67cf2af0cb40ec
Student_6 <codecat.ua@gmail.com>
2026-03-17 19:02:45 +0200

    запрограмували 3 астероїди

--- src/main/java/com/example/asteroids/HelloController.java
+++ src/main/java/com/example/asteroids/HelloController.java
@@ -2,6 +2,7 @@ package com.example.asteroids;
 
 import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -10,20 +11,30 @@ import javafx.scene.input.KeyEvent;
 public class HelloController {
 
     @FXML
-    private ImageView bg1, bg2, rocket, asteroid1, asteroid2, asteroid3;
+    private ImageView bg1, bg2, rocket, asteroid1, asteroid2, asteroid3, flame;
+
+    @FXML
+    private Group groupRocket;
 
     private double offset = 0;
-    private double offsetAsteroid1 = 0;
+    private double asteroid_1_x = 900 + Math.random()*300;
+    private double asteroid_2_x = 900 + Math.random()*300;
+    private double asteroid_3_x = 900 + Math.random()*300;
 
     @FXML
     public void initialize() {
         Image background = new Image(getClass().getResourceAsStream("/images/background.jpg"));
         Image imageAsteroid = new Image(getClass().getResourceAsStream("/images/asteroid.png"));
         rocket.setImage(new Image(getClass().getResourceAsStream("/images/rocket.png")));
+        flame.setImage(new Image(getClass().getResourceAsStream("/images/flame.png")));
 
         asteroid1.setImage(imageAsteroid);
-        asteroid1.setLayoutX(900);
         asteroid1.setLayoutY(Math.random()*540);
+        asteroid2.setImage(imageAsteroid);
+        asteroid2.setLayoutY(Math.random()*540);
+        asteroid3.setImage(imageAsteroid);
+        asteroid3.setLayoutY(Math.random()*540);
+
         bg1.setImage(background);
         bg2.setImage(background);
         bg2.setLayoutX(900);
@@ -37,12 +48,30 @@ public class HelloController {
                     offset = 0;
                 }
 
-                offsetAsteroid1 -= 2;
-                if (offsetAsteroid1 <= -1000){
-                    offsetAsteroid1 = 0;
-                }
-                asteroid1.setLayoutX(offsetAsteroid1+900);
+                asteroid_1_x -= 2;
+                asteroid_2_x -= 2.25;
+                asteroid_3_x -= 1.75;
 
+                if (asteroid_1_x <= -100) {
+                    asteroid_1_x = 900 + Math.random() * 300;
+                    asteroid1.setLayoutY(Math.random()*540);
+                }
+                asteroid1.setLayoutX(asteroid_1_x);
+                asteroid1.setRotate(asteroid_1_x);
+
+                if (asteroid_2_x <= -100) {
+                    asteroid_2_x = 900 + Math.random() * 300;
+                    asteroid2.setLayoutY(Math.random()*540);
+                }
+                asteroid2.setLayoutX(asteroid_2_x);
+                asteroid2.setRotate(asteroid_2_x);
+
+                if (asteroid_3_x <= -100) {
+                    asteroid_3_x = 900 + Math.random() * 300;
+                    asteroid3.setLayoutY(Math.random()*540);
+                }
+                asteroid3.setLayoutX(asteroid_3_x);
+                asteroid3.setRotate(asteroid_3_x);
                 bg1.setLayoutX(offset);
                 bg2.setLayoutX(offset + 900);
 
@@ -54,17 +83,17 @@ public class HelloController {
 
     public void handleKeyPressed(KeyEvent keyEvent) {
 
-        if (keyEvent.getCode() == KeyCode.UP && rocket.getLayoutY() >=0){
-            rocket.setLayoutY(rocket.getLayoutY() - 10);
-            rocket.setRotate(75);
+        if (keyEvent.getCode() == KeyCode.UP && groupRocket.getLayoutY() >=-300){
+            groupRocket.setLayoutY(groupRocket.getLayoutY() - 10);
+            groupRocket.setRotate(-15);
         }
-        if (keyEvent.getCode() == KeyCode.DOWN && rocket.getLayoutY() <=510){
-            rocket.setLayoutY(rocket.getLayoutY() + 10);
-            rocket.setRotate(105);
+        if (keyEvent.getCode() == KeyCode.DOWN && groupRocket.getLayoutY() <=240){
+            groupRocket.setLayoutY(groupRocket.getLayoutY() + 10);
+            groupRocket.setRotate(15);
         }
     }
 
     public void handleKeyReleased(KeyEvent keyEvent) {
-        rocket.setRotate(90);
+        groupRocket.setRotate(0);
     }
 }
