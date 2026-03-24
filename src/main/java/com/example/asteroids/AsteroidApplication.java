19ff817df0be8efb02b37cfac30a213d14af510b
Amid <codecat.ua@gmail.com>
2026-03-24 18:31:52 +0200

    створили інтерфейс меню

--- src/main/java/com/example/asteroids/AsteroidApplication.java
+++ src/main/java/com/example/asteroids/AsteroidApplication.java
@@ -10,15 +10,15 @@ import java.io.IOException;
 public class AsteroidApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AsteroidApplication.class.getResource("game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AsteroidApplication.class.getResource("mainMenu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("Asteroids");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
-        GameController controller = fxmlLoader.getController();
-        scene.setOnKeyPressed(controller::handleKeyPressed);
-        scene.setOnKeyReleased(controller::handleKeyReleased);
+//        GameController controller = fxmlLoader.getController();
+//        scene.setOnKeyPressed(controller::handleKeyPressed);
+//        scene.setOnKeyReleased(controller::handleKeyReleased);
     }
 
     public static void main(String[] args) {
